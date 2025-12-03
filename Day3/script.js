ada65688478818ff82b026ee3ef6ae0086afd66e
nishanththiru77 <nishanththiru77@gmail.com>
2025-12-03 23:12:08 +0530

    Add files via upload

---
+++ Day3/script.js
@@ -0,0 +1,25 @@
+var a=10;
+console.log(a);
+var a=20;
+console.log(a);
+a=30;
+console.log(a);
+let b=15;
+console.log(b);
+b=20;
+console.log(b);
+const c=60;
+console.log(c);
+function greet()
+{
+    console.log("Good Morning!");
+}
+greet();
+function add(a,b)
+{
+    let sum = a+b;
+     console.log(sum);
+}
+
+add(5,2);
+
